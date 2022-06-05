e0daa110b90b7e14b00deae8a66608c3114058eb
yuxianxu <yuxian.xu@gmail.com>
2022-06-04 23:14:12 -0700

    Add event listener

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 class App {
   constructor() {
-    console.log('app works!');
+    
+    this.addEventListeners()
   }
+
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+        this.handleFormClick(event)
+    })
+  }
+
+  handleFormClick(event) {
+      const isFormClicked = this.$form.contains(event.target)
+
+      if(isFormClicked) {
+          this.openForm()
+      } else {
+          this.closeForm()
+      }
+  }
+
+
+
+
 }
 
 new App();
