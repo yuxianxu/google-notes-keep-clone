e28f016f450c437355fec2b69b0463e7fdf9a802
yuxianxu <yuxian.xu@gmail.com>
2022-06-05 12:08:31 -0700

    Add display notes to dom

--- app.js
+++ app.js
@@ -1,70 +1,92 @@
 class App {
   constructor() {
-    this.notes = []  
+    this.notes = [];
 
-    this.$form = document.querySelector('#form')
-    this.$noteTitle = document.querySelector('#note-title')
-    this.$noteText = document.querySelector('#note-text')
-    this.$formButtons = document.querySelector('#form-buttons')
-    
-    this.addEventListeners()
+    this.$form = document.querySelector('#form');
+    this.$noteTitle = document.querySelector('#note-title');
+    this.$noteText = document.querySelector('#note-text');
+    this.$formButtons = document.querySelector('#form-buttons');
+    this.$notes = document.querySelector('#notes');
+    this.$placeholder = document.querySelector('#placeholder');
+
+    this.addEventListeners();
   }
 
   addEventListeners() {
-    window.addEventListener('click', event => {
-        this.handleFormClick(event)
-    })
+    window.addEventListener('click', (event) => {
+      this.handleFormClick(event);
+    });
 
-    this.$form.addEventListener('submit', event => {
-        event.preventDefault()
+    this.$form.addEventListener('submit', (event) => {
+      event.preventDefault();
 
-        const title = this.$noteTitle.value
-        const text = this.$noteText.value
-        const hasNote = title || text
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
 
-        if(hasNote) {
-            this.addNote({title, text})
-        }
-    })
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
+    });
   }
 
   handleFormClick(event) {
-      const isFormClicked = this.$form.contains(event.target)
+    const isFormClicked = this.$form.contains(event.target);
 
-      if(isFormClicked) {
-          this.openForm()
-      } else {
-          this.closeForm()
-      }
+    if (isFormClicked) {
+      this.openForm();
+    } else {
+      this.closeForm();
+    }
   }
 
   openForm() {
-      this.$form.classList.add('form-open')
-      this.$noteTitle.style.display = 'block'
-      this.$formButtons.style.display = 'block'
+    this.$form.classList.add('form-open');
+    this.$noteTitle.style.display = 'block';
+    this.$formButtons.style.display = 'block';
   }
 
   closeForm() {
-    this.$form.classList.remove('form-open')
-    this.$noteTitle.style.display = 'none'
-    this.$formButtons.style.display = 'none'
+    this.$form.classList.remove('form-open');
+    this.$noteTitle.style.display = 'none';
+    this.$formButtons.style.display = 'none';
+    this.$noteText.value = '';
+    this.$noteTitle.value = '';
   }
 
   addNote(note) {
-
     const newNote = {
-        title: note.title,
-        text: note.text,
-        color: 'white',
-        id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
-    }
-    this.notes = [...this.notes, newNote]
-    console.log(this.notes);
-
+      title: note.title,
+      text: note.text,
+      color: 'salmon',
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+    };
+    this.notes = [...this.notes, newNote];
+    this.displayNotes();
+    this.closeForm();
   }
 
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
 
+    this.$notes.innerHTML = this.notes.map(
+      (note) => `
+        <div style='background: ${note.color}' class='note'>
+            <div class='${note.title && 'note-title'}'>${note.title}</div>
+            <div class='note-text'>${note.text}</div>
+            <div class='toolbar-container'>
+                <div class='toolbar'>
+                <svg class='toolbar-delete' data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:var(--geist-foreground)"><path d="M3 6h18"/><path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/><path d="M10 11v6"/><path d="M14 11v6"/></svg>
+                
+                </div>
+            </div>
+        </div>    
+    `
+    ).join('');
 
+    console.log(hasNotes);
+  }
 }
 
 new App();
