d8305efb8a87b135bdd9966670384cc84534791c
yuxianxu <yuxian.xu@gmail.com>
2022-06-05 11:14:15 -0700

    Add add note function

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 class App {
   constructor() {
+    this.notes = []  
 
     this.$form = document.querySelector('#form')
     this.$noteTitle = document.querySelector('#note-title')
+    this.$noteText = document.querySelector('#note-text')
     this.$formButtons = document.querySelector('#form-buttons')
     
     this.addEventListeners()
   }
 
   addEventListeners() {
-    document.body.addEventListener('click', event => {
+    window.addEventListener('click', event => {
         this.handleFormClick(event)
+    })
+
+    this.$form.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const title = this.$noteTitle.value
+        const text = this.$noteText.value
+        const hasNote = title || text
+
+        if(hasNote) {
+            this.addNote({title, text})
+        }
     })
   }
 
@@ -36,8 +50,18 @@ class App {
     this.$formButtons.style.display = 'none'
   }
 
+  addNote(note) {
 
+    const newNote = {
+        title: note.title,
+        text: note.text,
+        color: 'white',
+        id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    }
+    this.notes = [...this.notes, newNote]
+    console.log(this.notes);
 
+  }
 
 
 
