bcfbf3df25d5f6e74dd0c408515b86ce1cd21173
yuxianxu <yuxian.xu@gmail.com>
2022-06-06 15:02:19 -0700

    Add tool bar to edit note background

--- app.js
+++ app.js
@@ -8,13 +8,44 @@ class App {
     this.$formButtons = document.querySelector('#form-buttons');
     this.$notes = document.querySelector('#notes');
     this.$placeholder = document.querySelector('#placeholder');
-
+    this.$closeForm = document.querySelector('#form-close-button');
+    this.$modal = document.querySelector('.modal');
+    this.$modalTitle = document.querySelector('.modal-title');
+    this.$modalText = document.querySelector('.modal-text');
+    this.$modalCloseButton = document.querySelector('.modal-close-button');
+    this.$colorTooltip = document.querySelector('#color-tooltip');
     this.addEventListeners();
   }
 
   addEventListeners() {
     window.addEventListener('click', (event) => {
       this.handleFormClick(event);
+      this.selectedNote(event);
+      this.openModal(event);
+    });
+
+    document.body.addEventListener('mouseover', (event) => {
+      this.openTooltip(event);
+    });
+
+    document.body.addEventListener('mouseout', (event) => {
+      this.closeTooltip(event);
+    });
+
+    this.$colorTooltip.addEventListener('mouseover', function () {
+      this.style.display = 'flex';
+    });
+
+    this.$colorTooltip.addEventListener('mouseout', function () {
+      this.style.display = 'none';
+    });
+
+    this.$colorTooltip.addEventListener('click', (event) => {
+      const color = event.target.dataset.color;
+      if (color) {
+        this.editNoteColor(color);
+        console.log(color)
+      }
     });
 
     this.$form.addEventListener('submit', (event) => {
@@ -28,13 +59,28 @@ class App {
         this.addNote({ title, text });
       }
     });
+
+    this.$closeForm.addEventListener('click', (event) => {
+      event.stopPropagation();
+      this.closeForm();
+    });
+
+    this.$modalCloseButton.addEventListener('click', (event) => {
+      this.closeModal(event);
+    });
   }
 
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
 
+    const title = this.$noteTitle.value;
+    const text = this.$noteText.value;
+    const hasNote = title || text;
+
     if (isFormClicked) {
       this.openForm();
+    } else if (hasNote) {
+      this.addNote({ title, text });
     } else {
       this.closeForm();
     }
@@ -54,10 +100,38 @@ class App {
     this.$noteTitle.value = '';
   }
 
-  addNote(note) {
+  openModal(event) {
+    if (event.target.closest('.note')) {
+      this.$modal.classList.toggle('open-modal');
+      this.$modalTitle.value = this.title;
+      this.$modalText.value = this.text;
+    }
+  }
+
+  closeModal(event) {
+    this.editNote();
+    this.$modal.classList.toggle('open-modal');
+  }
+
+  openTooltip(event) {
+    if (!event.target.matches('.toolbar-color')) return;
+    this.id = event.target.dataset.id;
+    const noteCoords = event.target.getBoundingClientRect();
+    const horizontal = noteCoords.left + window.scrollX;
+    const vertical =  window.scrollY - 20;
+    this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`;
+    this.$colorTooltip.style.display = 'flex';
+  }
+
+  closeTooltip(event) {
+    if (!event.target.matches('.toolbar-color')) return;
+    this.$colorTooltip.style.display = 'none';
+  }
+
+  addNote({ title, text }) {
     const newNote = {
-      title: note.title,
-      text: note.text,
+      title,
+      text,
       color: '#fbbc04',
       id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
     };
@@ -66,26 +140,67 @@ class App {
     this.closeForm();
   }
 
+  editNote() {
+    const title = this.$modalTitle.value;
+    const text = this.$modalText.value;
+    this.notes = this.notes.map((note) =>
+      note.id === Number(this.id) ? { ...note, title, text } : note
+    );
+    this.displayNotes();
+  }
+
+  editNoteColor(color) {
+    this.notes = this.notes.map((note) =>
+      note.id === Number(this.id) ? { ...note, color } : note
+    );
+    this.displayNotes();
+  }
+
+  selectedNote(event) {
+    const $selectedNote = event.target.closest('.note');
+    if (!$selectedNote) return;
+    const [$noteTitle, $noteText] = $selectedNote.children;
+    this.title = $noteTitle.innerText;
+    this.text = $noteText.innerText;
+    this.id = $selectedNote.dataset.id;
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
 
-    this.$notes.innerHTML = this.notes.map(
-      (note) => `
-        <div style='background: ${note.color}' class='note'>
-            <div class='${note.title && 'note-title'}'>${note.title}</div>
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) => `
+          <div style='background: ${note.color}' class='note' data-id='${
+          note.id
+        }'>
+            <div class='${note.title && ' note-title'}'>${note.title}</div>
             <div class='note-text'>${note.text}</div>
             <div class='toolbar-container'>
                 <div class='toolbar'>
-                <svg class='toolbar-delete' data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24" style="color:var(--geist-foreground)"><path d="M3 6h18"/><path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2"/><path d="M10 11v6"/><path d="M14 11v6"/></svg>
-                
+                    <svg class="toolbar-color"  data-id=${note.id} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#ffffff">
+                    <path d="M12 22C6.49 22 2 17.51 2 12S6.49 2 12 2s10 4.04 10 9c0 3.31-2.69 6-6 6h-1.77c-.28 0-.5.22-.5.5 0 .12.05.23.13.33.41.47.64 1.06.64 1.67A2.5 2.5 0 0 1 12 22zm0-18c-4.41 0-8 3.59-8 8s3.59 8 8 8c.28 0 .5-.22.5-.5a.54.54 0 0 0-.14-.35c-.41-.46-.63-1.05-.63-1.65a2.5 2.5 0 0 1 2.5-2.5H16c2.21 0 4-1.79 4-4 0-3.86-3.59-7-8-7z"/><circle cx="6.5" cy="11.5" r="1.5"/>
+                    <circle cx="9.5" cy="7.5" r="1.5"/><circle cx="14.5" cy="7.5" r="1.5"/><circle cx="17.5" cy="11.5" r="1.5"/>
+                    </svg>
+
+                    <svg class='toolbar-delete' data-testid="geist-icon" fill="none" height="24"
+                        shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"
+                        style="color:var(--geist-foreground)">
+                        <path d="M3 6h18" />
+                        <path d="M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2" />
+                        <path d="M10 11v6" />
+                        <path d="M14 11v6" />
+                    </svg>
+
                 </div>
             </div>
-        </div>    
+        </div>
     `
-    ).join('');
+      )
+      .join('');
 
-    console.log(hasNotes);
   }
 }
 
